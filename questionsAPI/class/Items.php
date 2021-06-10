ebc99c0e4e742f1c22c62417e696e934bde45021
PricopCosminFII <79044611+PricopCosminFII@users.noreply.github.com>
2021-06-10 17:06:19 +0300

    Update api la noua structura de tabele de intrebari

--- questionsAPI/class/Items.php
+++ questionsAPI/class/Items.php
@@ -2,25 +2,25 @@
 class Items
 {
 
-	private $itemsTable = "questions";
-	public $id;
+	private $itemsTable;
+	public $lecture;
 	public $question;
-	public $answear;
+	public $option1;
+	public $option2;
+	public $option3;
+    public $answear;
 	private $conn;
 
-	public function __construct($db)
+	public function __construct($db,$name)
 	{
 		$this->conn = $db;
+		$this->itemsTable=$name;
 	}
 
 	function read()
 	{
-		if ($this->id) {
-			$stmt = $this->conn->prepare("SELECT * FROM " . $this->itemsTable . " WHERE id = ?");
-			$stmt->bind_param("i", $this->id);
-		} else {
-			$stmt = $this->conn->prepare("SELECT * FROM " . $this->itemsTable);
-		}
+	
+		$stmt = $this->conn->prepare("SELECT * FROM " . $this->itemsTable);
 		$stmt->execute();
 		$result = $stmt->get_result();
 		return $result;
@@ -29,15 +29,18 @@ class Items
 	function create(){
 		
 		$stmt = $this->conn->prepare("
-			INSERT INTO questions (`id`, `question`, `answear`)
-			VALUES(?,?,?)");
+			INSERT INTO ".$this->itemsTable. "(`lecture`, `question`, `option1`,`option2`,`option3`,`answear`)
+			VALUES(?,?,?,?,?,?)");
 		
-		$this->id = htmlspecialchars(strip_tags($this->id));
+		$this->lecture = htmlspecialchars(strip_tags($this->lecture));
 		$this->question = htmlspecialchars(strip_tags($this->question));
+		$this->option1 = htmlspecialchars(strip_tags($this->option1));
+		$this->option2 = htmlspecialchars(strip_tags($this->option2));
+		$this->option3 = htmlspecialchars(strip_tags($this->option3));
 		$this->answear = htmlspecialchars(strip_tags($this->answear));
 		
 		
-		$stmt->bind_param("iss", $this->id, $this->question, $this->answear);
+		$stmt->bind_param("ssssss", $this->lecture, $this->question,$this->option1,$this->option2,$this->option3, $this->answear);
 		
 		if($stmt->execute()){
 			return true;
---
+++ questionsAPI/itemsCSS/read.php
@@ -0,0 +1,38 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/Database.php';
+include_once '../class/Items.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$items = new Items($db,"questionscss");
+
+$result = $items->read();
+
+if ($result->num_rows > 0) {
+    $itemRecords = array();
+    $itemRecords["items"] = array();
+    while ($item = $result->fetch_assoc()) {
+        extract($item);
+        $itemDetails = array(
+            "lecture" => $lecture,
+            "question" => $question,
+            "option1" => $option1,
+            "option2" => $option2,
+            "option3" => $option3,
+            "answear" => $answear
+
+        );
+        array_push($itemRecords["items"], $itemDetails);
+    }
+    http_response_code(200);
+    echo json_encode($itemRecords);
+} else {
+    http_response_code(404);
+    echo json_encode(
+        array("message" => "No item found.")
+    );
+}
